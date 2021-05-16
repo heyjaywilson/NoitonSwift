3b94f475c8f99679f8678bf6477553990d05c448
maegan <maegan@maeganwilson.com>
2021-05-16 16:34:12 -0500

    Added some models

--- notionintegration/notionintegration/NotionModels.swift
+++ notionintegration/notionintegration/NotionModels.swift
@@ -7,6 +7,47 @@
 
 import Foundation
 
-struct NotionDB{
+struct NotionDB: Codable{
+    let object = "database"
+    var id: String
+    let created: String
+    var lastEdited: String
+    var title: [RichTextObject]
+    var properties: PropertyObject
     
+    enum CodingKeys: String, CodingKey {
+        case id
+        case created = "created_time"
+        case lastEdited = "last_edited_time"
+        case title
+        case properties
+    }
+}
+
+struct RichTextObject: Codable {
+    let plainText: String
+    var href: String?
+    var annotations: AnnotationObject
+    var type: String
+    
+    enum CodingKeys: String, CodingKey {
+        case plainText = "plain_text"
+        case href
+        case annotations
+        case type
+    }
+}
+
+struct PropertyObject: Codable {
+    var id: String
+    var type: String
+}
+
+struct AnnotationObject: Codable {
+    var bold: Bool
+    var italic: Bool
+    var strikethrough: Bool
+    var underline: Bool
+    var code: Bool
+    var color: String
 }
