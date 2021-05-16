66c4a7f0cfb7a980220f7c44d5e9abebcff57add
Maegan <maegan@maeganwilson.com>
2021-05-16 13:53:09 -0500

    Making test app

---
+++ notionintegration/notionintegration/notionintegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  notionintegrationApp.swift
+//  notionintegration
+//
+//  Created by Maegan Wilson on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct notionintegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
